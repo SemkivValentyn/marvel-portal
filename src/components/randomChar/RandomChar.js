e354ba974a8bcb59bee83bdf0726ade082b1213a
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-09-08 00:15:41 +0300

    RandomChar in a functional component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,97 +1,105 @@
-import { useState, useEffect } from "react";
-import MarvelService from "../../services/MarvelService";
-import Spinner from "../spinner/Spinner";
-import ErrorMessage from "../errorMessage/ErrorMessage";
+import { useState, useEffect } from 'react'
+import MarvelService from '../../services/MarvelService'
+import Spinner from '../spinner/Spinner'
+import ErrorMessage from '../errorMessage/ErrorMessage'
 
-import "./randomChar.scss";
-import mjolnir from "../../resources/img/mjolnir.png";
+import './randomChar.scss'
+import mjolnir from '../../resources/img/mjolnir.png'
 
 const RandomChar = () => {
-  const [char, setChar] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+    const [char, setChar] = useState({})
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
 
-  const marvelService = new MarvelService();
+    const marvelService = new MarvelService()
 
-  useEffect(() => {
-    updateChar();
-    // eslint-disable-next-line
-  }, []);
+    useEffect(() => {
+        updateChar()
+        // eslint-disable-next-line
+    }, [])
 
-  const onCharLoader = (char) => {
-    setChar(char);
-    setLoading(false);
-  };
+    const onCharLoader = (char) => {
+        setChar(char)
+        setLoading(false)
+    }
 
-  const onError = () => {
-    setError(true);
-    setLoading(false);
-  };
+    const onError = () => {
+        setError(true)
+        setLoading(false)
+    }
 
-  const updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    marvelService.getCharacter(id).then(onCharLoader).catch(onError);
-  };
+    const updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+        marvelService.getCharacter(id)
+            .then(onCharLoader)
+            .catch(onError)
+    }
 
+    const errorMessage = error ? <ErrorMessage /> : null
+    const spinner = loading ? <Spinner /> : null
+    const contetn = !(loading || error) ? <View char={char} /> : null
 
-  const errorMessage = error ? <ErrorMessage /> : null;
-  const spinner = loading ? <Spinner /> : null;
-  const contetn = !(loading || error) ? <View char={char} /> : null;
+    return (
+        <div className="randomchar">
+            {errorMessage}
+            {spinner}
+            {contetn}
 
-  return (
-    <div className="randomchar">
-      {errorMessage}
-      {spinner}
-      {contetn}
-
-      <div className="randomchar__static">
-        <p className="randomchar__title">
-          Random character for today!
-          <br />
-          Do you want to get to know him better?
-        </p>
-        <p className="randomchar__title">Or choose another one</p>
-        <button className="button button__main " onClick={updateChar}>
-          <div className="inner">try it</div>
-        </button>
-        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration " />
-      </div>
-    </div>
-  );
-};
+            <div className="randomchar__static">
+                <p className="randomchar__title">
+                    Random character for today!
+                    <br />
+                    Do you want to get to know him better?
+                </p>
+                <p className="randomchar__title">Or choose another one</p>
+                <button className="button button__main " onClick={updateChar}>
+                    <div className="inner">try it</div>
+                </button>
+                <img
+                    src={mjolnir}
+                    alt="mjolnir"
+                    className="randomchar__decoration "
+                />
+            </div>
+        </div>
+    )
+}
 
 const View = ({ char }) => {
-  const { name, description, thumbnail, homepage, wiki } = char;
-  let imgStyle = { objectFit: "cover" };
-  if (
-    thumbnail ===
-    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-  ) {
-    imgStyle = { objectFit: "contain" };
-  }
+    const { name, description, thumbnail, homepage, wiki } = char
+    let imgStyle = { objectFit: 'cover' }
+    if (
+        thumbnail ===
+        'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+    ) {
+        imgStyle = { objectFit: 'contain' }
+    }
 
-  return (
-    <div className="randomchar__block">
-      <img
-        src={thumbnail}
-        alt="Random character"
-        className="randomchar__img"
-        style={imgStyle}
-      />
-      <div className="randomchar__info">
-        <p className="randomchar__name">{name}</p>
-        <p className="randomchar__descr">{description}</p>
-        <div className="randomchar__btns">
-          <a href={homepage} className="button button__main btn-dark">
-            <div className="inner">homepage</div>
-          </a>
-          <a href={wiki} className="button button__secondary btn-dark">
-            <div className="inner">Wiki</div>
-          </a>
+    return (
+        <div className="randomchar__block">
+            <img
+                src={thumbnail}
+                alt="Random character"
+                className="randomchar__img"
+                style={imgStyle}
+            />
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">{description}</p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main btn-dark">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a
+                        href={wiki}
+                        className="button button__secondary btn-dark"
+                    >
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default RandomChar;
+export default RandomChar
