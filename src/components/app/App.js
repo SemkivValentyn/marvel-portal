c07b38d9d80032136e2bb09114176bacf6b4e598
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-09-07 23:27:52 +0300

    App in a functional component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
@@ -6,42 +6,31 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
+const App = () => {
+  const [selectedChar, setSelectedChar] = useState(null);
 
+  const onCharSelected = (id) => {
+    setSelectedChar(id);
+  };
 
-class App extends Component {
-
-    state = {
-        selectedChar: null
-    }
-
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id,
-        })
-    }
-
-
-    render() {
-        return (
-            <div className="app">
-                <AppHeader />
-                <main>
-                   <ErrorBoundary>
-                        <RandomChar />
-                   </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected} /> 
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                            <CharInfo charId={this.state.selectedChar} />
-                        </ErrorBoundary>
-                    </div>
-    
-                </main>
-            </div>
-        )
-    }
-}
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <ErrorBoundary>
+          <RandomChar />
+        </ErrorBoundary>
+        <div className="char__content">
+          <ErrorBoundary>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
