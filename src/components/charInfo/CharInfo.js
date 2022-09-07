8610f41255edd50b06941ae6d5ce6b2e4521026f
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-09-08 00:00:19 +0300

    CharInfo in a functional component

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,138 +1,107 @@
-import { Component } from 'react';
-import MarvelService from '../../services/MarvelService';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import Spinner from '../spinner/Spinner';
-import Skeleton from '../skeleton/Skeleton';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
 
-class CharInfo extends Component {
+const CharInfo = (props) => {
 
-    state = {
-        char: null,
-        loading: false,
-        error: false
-    }
+    const [char, setChar] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
+    const marvelService = new MarvelService();
 
-    marvelService = new MarvelService();
+    useEffect(() => {
+        updateChar()
+        // eslint-disable-next-line
+    }, [props.charId])
 
-    componentDidMount() {
-        this.updateChar()
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.charId !== prevProps.charId) {
-            this.updateChar();
-        }
-    }
-
-
-    updateChar = () => {
-        const { charId } = this.props;
+    const updateChar = () => {
+        const {charId} = props;
         if (!charId) {
             return;
         }
-
-        this.onCharLoading();
-
-        this.marvelService
-            .getCharacter(charId)
-            .then(this.onCharLoader)
-            .catch(this.onError);
+        onCharLoading();
+        marvelService.getCharacter(charId)
+            .then(onCharLoaded)
+            .catch(onError)
     }
 
-
-
-    onCharLoader = (char) => {
-        this.setState({
-            char,
-            loading: false
-        })
+    const onCharLoaded = (char) => {
+        setLoading(false);
+        setChar(char);
     }
 
-    onCharLoading = () => {
-        this.setState({
-            loading: false,
-        })
+    const onCharLoading = () => {
+        setLoading(true);
     }
 
-
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
+    const onError = () => {
+        setError(true);
+        setLoading(false);
     }
 
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !char) ? <View char={char}/> : null;
 
-    render() {
-        const { char, loading, error } = this.state;
-
-
-        const skeleton = char || loading || error ? null : <Skeleton/> 
-        const errorMessage = error ? <ErrorMessage /> : null;
-        const spinner = loading ? <Spinner /> : null;
-        const contetn = !(loading || error || !char) ? < View char={char} /> : null;
-
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {contetn}
-            </div>
-        )
-    }
-
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
 }
 
-const View = ({ char }) => {
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
 
-    const { name, description, thumbnail, homepage, wiki, comics } = char
-
-
-    let imgStyle = { 'objectFit': 'cover' };
+    let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-        imgStyle = { 'objectFit': 'contain' };
+        imgStyle = {'objectFit' : 'contain'};
     }
+
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name}  style={imgStyle}/>
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
-                        <a href={homepage} className="button button__main btn-dark">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href={wiki} className="button button__secondary btn-dark">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
                 </div>
             </div>
-
             <div className="char__descr">
                 {description}
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
-
-                {comics.length > 0 ? null : 'There is no comic book at the moment'}
-
+                {comics.length > 0 ? null : 'There is no comics with this character'}
                 {
-                    comics.map((item, index) => {
+                    comics.map((item, i) => {
+                        // eslint-disable-next-line
+                        if (i > 9) return;
                         return (
-                            <li key={index} className="char__comics-item">
-                               {item.name}
+                            <li key={i} className="char__comics-item">
+                                {item.name}
                             </li>
                         )
                     })
-                }
-
-                
+                }                
             </ul>
         </>
     )
