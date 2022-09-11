4672e1b2e7e0949a3850bd50e57517c340856912
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-09-11 21:52:38 +0300

    get comics

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -20,6 +20,18 @@ const MarvelService = () => {
     return _transformCharacter(res.data.results[0]);
   };
 
+  const getAllComics = async (offset = 0) => {
+    const res = await request(
+      `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+    );
+    return res.data.results.map(_transformComics);
+  };
+
+  const getComics = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
+  };
+
   const _transformCharacter = (char) => {
     return {
       id: char.id,
@@ -34,7 +46,19 @@ const MarvelService = () => {
     };
   };
 
-  return { loading, error, getAllCharacters, getCharacter, clearError };
+  const _transformComics = (comics) => {
+    return {
+        id: comics.id,
+        title: comics.title,
+        description: comics.description || 'There is no description',
+        pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+        thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+        language: comics.textObjects.language || 'en-us',
+        price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+    }
+}
+
+  return { loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComics };
 };
 
 export default MarvelService;
