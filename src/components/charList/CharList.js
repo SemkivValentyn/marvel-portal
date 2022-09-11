fbbf12080cce8fff9c6fbcde92b47ad2d023ace4
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-09-11 20:23:53 +0300

    hooks component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -66,13 +66,12 @@ const CharList = (props) => {
 
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading && !newItemLoading ?  <Spinner /> : null;
-  const content = !(loading || error) ? items : null;
 
   return (
     <div className="char__list">
       {errorMessage}
       {spinner}
-      {content}
+      {items}
       <button
         className="button button__main button__long btn-char-list btn-dark"
         disabled={newItemLoading}
